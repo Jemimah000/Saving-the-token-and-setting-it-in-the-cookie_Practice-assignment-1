b63b9f0bfad8bf906cddaa9d4ccebcb2072d81c0
Jemimah000 <28dora.nobi@gmail.com>
2025-04-05 09:32:06 +0530

    updated

--- index.js
+++
@@ -1,9 +0,0 @@
-const { encrypt, decrypt } = require('./script');
-
-const myData = { name: "John", role: "student" };
-
-const token = encrypt(myData);
-console.log("Encrypted Token:", token);
-
-const data = decrypt(token);
-console.log("Decrypted Data:", data);
